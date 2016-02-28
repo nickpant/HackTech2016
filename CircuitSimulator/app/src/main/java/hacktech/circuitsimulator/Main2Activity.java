b879fd617269dbf80c9175efd59abb1c58080c38
John Wu <john.wu@mail.mcgill.ca>
2016-02-27 16:48:54 -0800

    made refresh button

--- CircuitSimulator/app/src/main/java/hacktech/circuitsimulator/Main2Activity.java
+++ CircuitSimulator/app/src/main/java/hacktech/circuitsimulator/Main2Activity.java
@@ -1,7 +1,10 @@
 package hacktech.circuitsimulator;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import java.io.File;
 import android.graphics.Bitmap;
@@ -20,5 +23,13 @@ public class Main2Activity extends AppCompatActivity {
             ImageView myImage = (ImageView) findViewById(R.id.image);
             myImage.setImageBitmap(myBitmap);
         }
+
+        Button refreshButton = (Button)findViewById(R.id.refresh_button);
+        refreshButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Main2Activity.this, MainActivity.class));
+            }
+        });
     }
 }
