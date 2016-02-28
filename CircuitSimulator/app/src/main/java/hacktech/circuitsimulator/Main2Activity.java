58f1649803da7442038ec0b6e037d1925344c35c
John Wu <john.wu@mail.mcgill.ca>
2016-02-27 16:23:07 -0800

    updated second screen

--- CircuitSimulator/app/src/main/java/hacktech/circuitsimulator/Main2Activity.java
+++ CircuitSimulator/app/src/main/java/hacktech/circuitsimulator/Main2Activity.java
@@ -2,6 +2,10 @@ package hacktech.circuitsimulator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import java.io.File;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 
 public class Main2Activity extends AppCompatActivity {
 
@@ -9,5 +13,12 @@ public class Main2Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        File imgFile = new  File("/sdcard/Circuit Simulator/cam_image.jpg");
+
+        if(imgFile.exists()){
+            Bitmap myBitmap = BitmapFactory.decodeFile(imgFile.getAbsolutePath());
+            ImageView myImage = (ImageView) findViewById(R.id.image);
+            myImage.setImageBitmap(myBitmap);
+        }
     }
 }
